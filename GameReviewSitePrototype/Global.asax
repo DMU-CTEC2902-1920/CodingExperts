1d5ef32ac2aa922b547dfe10228d1f01f3a19108
p2415646 <p2415646@GWHXX473WIN003W>
2020-02-17 17:12:13 +0000

    Add project files.

---
+++ GameReviewSitePrototype/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GameReviewSitePrototype.MvcApplication" Language="C#" %>
---
+++ GameReviewSitePrototype/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GameReviewSitePrototype.Startup))]
+namespace GameReviewSitePrototype
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
