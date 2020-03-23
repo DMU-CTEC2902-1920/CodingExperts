152e2025572af3ee612f817603bc5b1366cd7744
P2441751 <P2441751@GWHXX473WIN002W>
2020-03-23 15:38:25 +0000

    Chaning classes and adding new pages.

---
+++ GameReviewSitePrototype/Controllers/ShopController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameReviewSitePrototype.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ViewResult Index()
+        {
+            ViewBag.Title = "The Shop";
+            ViewBag.WelcomeMessage = "Welcome to the games shop. Kindly spend all your money with us!!";
+            ViewData["CurrentTime"] = DateTime.Now;
+            return View(); 
+        }
+
+        public string Product(int id)
+        {
+            return "Product: Id = " + id.ToString();
+        }
+
+        public RedirectResult GameCD()
+        {
+            return Redirect("~/Shop/Product");
+        }
+    }
+}
