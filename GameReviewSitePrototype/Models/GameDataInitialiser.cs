1072648636728159a5900ea58ce5260c468dc0a1
joseph fidzani <joefidzani@gmail.com>
2020-03-25 19:37:06 +0000

    Fixed issue with data initialise

--- GameReviewSitePrototype/Models/GameDataInitialiser.cs
+++ GameReviewSitePrototype/Models/GameDataInitialiser.cs
@@ -28,13 +28,13 @@ namespace GameReviewSitePrototype.Models
             Games game2 = new Games();
 
             //set game id data
-            game1.GameID = 2;
+            game2.GameID = 2;
             //set game name data
-            game1.GameName = "Halo";
+            game2.GameName = "Halo";
             //set game release date
             //game1.GameReleaseDate = Convert.ToDateTime("11/12/2005");
             //set game active
-            game1.GameActive = true;
+            game2.GameActive = true;
             //add
             context.Games.Add(game2);
 
@@ -59,15 +59,15 @@ namespace GameReviewSitePrototype.Models
             Review rev2 = new Review();
 
             //set review id
-            rev1.ReviewID = 2;
+            rev2.ReviewID = 2;
             //set game id
-            rev1.GameID = 2;
+            rev2.GameID = 2;
             //set the review title
-            rev1.ReviewTitle = "Halo is a very good game";
+            rev2.ReviewTitle = "Halo is a very good game";
             //set the review content
-            rev1.ReviewContent = "Smooth fps shooter with a variety of gamemodes and weapons.";
+            rev2.ReviewContent = "Smooth fps shooter with a variety of gamemodes and weapons.";
             //set the review score
-            rev1.ReviewScore = 4;
+            rev2.ReviewScore = 4;
             //set the review Time stamp
             //rev1.ReviewTimeStamp = Convert.ToDateTime(DateTime.Now);
             //add
