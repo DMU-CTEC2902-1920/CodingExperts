8fee454a9f6fd0281ec159e6bf4e6f306c5ec956
joseph fidzani <joefidzani@gmail.com>
2020-03-25 19:47:43 +0000

    Removed gamedatainitialiser

--- GameReviewSitePrototype/Global.asax.cs
+++ GameReviewSitePrototype/Global.asax.cs
@@ -14,7 +14,7 @@ namespace GameReviewSitePrototype
     {
         protected void Application_Start()
         {
-            Database.SetInitializer(new GameDataInitialiser());
+            //Database.SetInitializer(new GameDataInitialiser());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
